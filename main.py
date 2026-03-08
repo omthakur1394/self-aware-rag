1b9a766e50d03e128780591079253ad900932502
omthakur1394 <your_main_github_email@gmail.com>
2026-03-09 03:06:23 +0530

    Fix startup bottleneck with lazy import

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from src.graph import app_graph
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,19 +17,21 @@ class chat_bot(BaseModel):
     chat : str
     thread_id: str = "1"
 
+@app.get("/")
+async def root():
+    return {"message": "Self-Aware RAG API is live and running!"}
 
 @app.post("/chat")
 async def chat(request:chat_bot):
+    # THE FIX: Move the import here so it doesn't block startup!
+    from src.graph import app_graph
+    
     config = {"configurable": {"thread_id": request.thread_id}}
     res = app_graph.invoke({"question": request.chat}, config=config)
     return {
-    "res": res["answer"],
-    "sources": [doc.metadata.get("source", "unknown") for doc in res["retrieved_docs"]]}
-                             
+        "res": res["answer"],
+        "sources": [doc.metadata.get("source", "unknown") for doc in res["retrieved_docs"]]
+    }
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=False)
-
-
-
-
